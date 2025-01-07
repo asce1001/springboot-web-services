bab01530511524ae195735f6f898d2f0b1f0de11
Yatindra Mahadik <mahadik.yatindra@gmail.com>
2025-01-07 07:39:02 -0500

    apir-versioning

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -15,4 +15,34 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersion(){
         return new PersonV2(new Name("Yatindra", "Mahadik"));
     }
+
+    @GetMapping(path = "person", params = "version=1")
+    public PersonV1 getFirstVersionParam(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "person", params = "version=2")
+    public PersonV2 getSecondVersionParam(){
+        return new PersonV2(new Name("Yatindra", "Mahadik"));
+    }
+
+    @GetMapping(path = "person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionHeader(){
+        return new PersonV1("Yatindra Mahadik");
+    }
+
+    @GetMapping(path = "person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionHeader(){
+        return new PersonV2(new Name("Yatindra", "Mahadik"));
+    }
+
+    @GetMapping(path = "person/accept", produces = "application/company-v1+json")
+    public PersonV1 getFirstVersionAcceptHeader(){
+        return new PersonV1("Yatindra Mahadik");
+    }
+
+    @GetMapping(path = "person/accept", produces = "application/company-v2+json")
+    public PersonV2 getSecondVersionAcceptHeader(){
+        return new PersonV2(new Name("Yatindra", "Mahadik"));
+    }
 }
